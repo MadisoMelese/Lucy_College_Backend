7fb636cedc67057d8ba3b4dba3acb296ebc37259
Madiso Melese <madishamadiso00@gmail.com>
2025-12-22 10:07:21 +0300

    src/controllers/course.controller.js modified

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -11,12 +11,16 @@ export const getAll = async (req, res) => {
 };
 
 export const getOne = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const course = await CourseService.getCourseById(id);
-    if (!course) return errorResponse(res, "Course not found", 404);
-    return success(res, course);
-  } catch (err) {
-    return errorResponse(res, err.message);
-  }
+    try {
+        const { code } = req.params;
+        const course = await CourseService.findCourseByCode(code);
+
+        if (!course) {
+            return errorResponse(res, `Course with code '${code}' not found`, 404);
+        }
+
+        return success(res, course);
+    } catch (err) {
+        return errorResponse(res, err.message, 500);
+    }
 };
