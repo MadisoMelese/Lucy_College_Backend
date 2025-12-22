df116993ef1496040679997153318bb72f0a8bcc
Madiso Melese <madishamadiso00@gmail.com>
2025-12-22 10:09:13 +0300

    src/routes/website.routes.js web routes updated

--- src/routes/website.routes.js
+++ src/routes/website.routes.js
@@ -4,6 +4,8 @@ import * as CourseController from "../controllers/course.controller.js";
 import * as NewsController from "../controllers/news.controller.js";
 import * as InquiryController from "../controllers/inquiry.controller.js";
 import * as FacultyController from "../controllers/admin/faculty.controller.js";
+import * as CourseCtrl from "../controllers/admin/course.controller.js";
+import * as ProgramCtrl from "../controllers/admin/program.controller.js";
 
 const router = express.Router();
 
@@ -18,10 +20,17 @@ router.get("/:facultyCode/departments", DepartmentController.getByFacultyCode); 
 router.get("/departments/id/:id", DepartmentController.getOneById);  //fetching department by ID
 router.get("/departments/code/:departmentCode", DepartmentController.getOne); //fetching department by department code
 
+// programmes
+router.get("/programs", ProgramCtrl.list);
+
+router.get("/programs/:id", ProgramCtrl.getOne);
+router.get("/programs/departmentCode/:departmentCode", ProgramCtrl.findProgramByDepartmentCode);
+
 // Courses
 router.get("/courses", CourseController.getAll);
-router.get("/courses/:id", CourseController.getOne);
-
+router.get("/courses/:facultyCode/:departmentCode", CourseCtrl.list);
+router.get("/courses/:code", CourseController.getOne);
+ 
 // News
 router.get("/news", NewsController.getAll);
 router.get("/news/:id", NewsController.getOne);
