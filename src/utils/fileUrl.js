f6eb2ec9a52475d29a635f00794b035aae3f6f2f
Madiso Melese <madishamadiso00@gmail.com>
2025-12-22 16:05:41 +0300

    src/utils/fileUrl.js modified with delete files from local storage

--- src/utils/fileUrl.js
+++ src/utils/fileUrl.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+
 export const fileUrl = (req, filePath) => {
   return `${process.env.BASE_URL}/uploads/${filePath}`;
+};
+
+
+
+export const deleteFile = (filePath) => {
+    if (!filePath) return;
+    const fullPath = path.resolve(filePath);
+    if (fs.existsSync(fullPath)) {
+        fs.unlinkSync(fullPath);
+    }
 };
