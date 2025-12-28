17d8c56cd7eadfedc58dabc53affc2cc95924ba6
Madiso Melese <madishamadiso00@gmail.com>
2025-12-29 00:44:46 +0300

    src/routes/gallery.routes.js gallery route modified with upload folders

--- src/routes/gallery.routes.js
+++ src/routes/gallery.routes.js
@@ -2,8 +2,15 @@ import { Router } from "express";
 import { GalleryController } from "../controllers/gallery.controller.js";
 import authenticate from "../middlewares/auth.middleware.js";
 import roleMiddleware from "../middlewares/role.middleware.js";
+import { upload } from "../config/multer.js";
+
 
 const router = Router();
+
+const setGalleryFolder = (req, res, next) => {
+  req.uploadFolder = "gallery";
+  next();
+};
 
 // Public
 router.get("/", GalleryController.list);
@@ -12,15 +19,15 @@ router.get("/:id", GalleryController.getOne);
 // Admin
 const adminRoles = ["SUPERADMIN", "REGISTRAR"];
 router.post(
-  "/",
+  "/admin",
   authenticate,
-  roleMiddleware(adminRoles),
+  roleMiddleware(adminRoles), setGalleryFolder, upload.single("image"),
   GalleryController.create
 );
 router.put(
-  "/:id",
+  "/admin/:id",
   authenticate,
-  roleMiddleware(adminRoles),
+  roleMiddleware(adminRoles), setGalleryFolder, upload.single("image"),
   GalleryController.update
 );
 router.delete(
